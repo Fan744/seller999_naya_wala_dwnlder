047f6079501f48bb0e513c9aa8ed1eb558060b98
Fan744 <135441979+Fan744@users.noreply.github.com>
2025-11-01 15:19:08 +0530

    Update API credentials and configuration in vars.py

--- vars.py
+++ vars.py
@@ -3,11 +3,12 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "23916035"))
-API_HASH = environ.get("API_HASH", "b1d6e8c276e940e1aff7278749b0b4f6")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "1263635239"))
+API_ID = int(environ.get("API_ID", "29383825"))
+API_HASH = environ.get("API_HASH", "3362cbcff56b786d82202086baab0bb7")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8313201920:AAH1PfXk6b6sgBPNCT_H5AEMAhZETItO5gg")
+OWNER = int(environ.get("OWNER", "8453948641"))
 CREDIT = "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
 LOG_CHANNEL = "-1002631537674" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
